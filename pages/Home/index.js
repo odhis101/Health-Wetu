6aae16b54efe91e2c3f22d4000af97012053c618
odhis101 <joshodhiambo5@gmail.com>
2023-03-28 22:22:51 +0300

    fixed the home page

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, Image, TouchableOpacity, View,ScrollView,Button} from 'react-native';
+import { StyleSheet, Text, Image,TextInput, TouchableOpacity,FlatList , View,ScrollView,Button} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { styles } from "./styles";
 import OurData from "health-wetu/components/ourData/ourData.js"
@@ -10,38 +10,42 @@ import io from 'socket.io-client';
 
 
 const Home =({navigation})=> {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-    const socket = io('ws://192.168.0.29:8080', { transports: ['websocket'] });
 
-    console.log('Socket.IO,',socket);
+    const socket = io('ws://192.168.0.31:8080', { transports: ['websocket'] });
 
-
-    socket.on('connect', () => {
-      console.log('Socket.IO connected');
-    
-      // Send a message to the server after the connection is established
-      socket.emit('chat message', 'Hello, server!');
-    });
-    
-    socket.on('chat message', (message) => {
-      console.log('Socket.IO message received:', message);
-    });
-    
-    socket.on('disconnect', () => {
-      console.log('Socket.IO disconnected');
-    });
-    socket.on('connect_error', (error) => {
+    useEffect(() => {
+      socket.on('connect', () => {
+        console.log('Socket.IO connected');
+  
+        // Send a message to the server after the connection is established
+        socket.emit('chat message', 'Hello, server!');
+      });
+  
+      socket.on('chat message', (message) => {
+        console.log('Socket.IO message received:', message);
+        setMessage(message);
+      });
+  
+      socket.on('disconnect', () => {
+        console.log('Socket.IO disconnected');
+      });
+  
+      socket.on('connect_error', (error) => {
         console.error('Socket.IO connection error:', error);
       });
-      
+  
       socket.on('connect_timeout', (timeout) => {
         console.error('Socket.IO connection timeout:', timeout);
       });
-      
+  
       socket.on('error', (error) => {
         console.error('Socket.IO error:', error);
       });
-    
+    }, []);
     
     
     
@@ -56,7 +60,8 @@ const Home =({navigation})=> {
   }
   const handleClick = () => {
     console.log('Sending message to server');
-    socket.emit('chat message', 'Hello, server!');
+    socket.emit('chat message', { text: inputValue });
+    setInputValue('');
 };
  console.log('first log ever ')
  
@@ -77,6 +82,14 @@ const Home =({navigation})=> {
 
 
             <ModuleButton text = 'calling for a friend '/>
+     <FlatList
+  data={messages}
+  renderItem={({ item }) => <Text>{item.text}</Text>}
+  keyExtractor={(item, index) => index.toString()}
+/>
+            <Text>{message.text}</Text>
+            <TextInput value={inputValue} onChangeText={setInputValue} />
+
             <Button title="Send message to server" onPress={handleClick} />
 
 
